585d4ec393f48bd58507425912b4a5880bf5e929
Jag Gangishetti <jagadeesh.g07@gmail.com>
2017-08-17 23:29:15 -0400

    Initial commit

---
+++ src/main/java/com/majic/barber/Barber_101/Barber101Application.java
@@ -0,0 +1,12 @@
+package com.majic.barber.Barber_101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Barber101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Barber101Application.class, args);
+	}
+}
